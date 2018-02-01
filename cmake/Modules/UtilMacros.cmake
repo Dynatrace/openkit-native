85eed377cc70bbfb855e867a9386dcf891a0a80f
Johannes Baeuerle <johannes.baeuerle@dynatrace.com>
2018-02-01 15:49:50 +0100

    initial project structure
    using cmake
    including a test for curl

---
+++ cmake/Modules/UtilMacros.cmake
@@ -0,0 +1,42 @@
+macro(fix_default_compiler_settings_)
+  if (MSVC)
+    # For MSVC, CMake sets certain flags to defaults we want to override.
+    # This replacement code is taken from sample in the CMake Wiki at
+    # http://www.cmake.org/Wiki/CMake_FAQ#Dynamic_Replace.
+    foreach (flag_var
+             CMAKE_CXX_FLAGS CMAKE_CXX_FLAGS_DEBUG CMAKE_CXX_FLAGS_RELEASE
+             CMAKE_CXX_FLAGS_MINSIZEREL CMAKE_CXX_FLAGS_RELWITHDEBINFO
+			 CMAKE_C_FLAGS CMAKE_C_FLAGS_DEBUG CMAKE_C_FLAGS_RELEASE
+             CMAKE_C_FLAGS_MINSIZEREL CMAKE_C_FLAGS_RELWITHDEBINFO
+			 )
+      # We prefer more strict warning checking for building
+      # Replaces /W3 with /W4 in defaults.
+      string(REPLACE "/W3" "/W4" ${flag_var} "${${flag_var}}")
+    endforeach()
+  endif()
+endmacro()
+
+macro(check_for_cxx_version_)
+	include(CheckCXXCompilerFlag)
+	##beginning with CMake version 3.1 this check would be possible with 
+	#set_property(TARGET prog PROPERTY CXX_STANDARD 11)
+	#set_property(TARGET prog PROPERTY CXX_STANDARD_REQUIRED ON)
+	if(MSVC)
+		CHECK_CXX_COMPILER_FLAG("/std:c++14" COMPILER_SUPPORTS_CXX14_VC)#no c++11 flag for Visual C++ compiler
+		if(COMPILER_SUPPORTS_CXX14_VC)
+			set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} /std:c++14")
+		else()
+				message(FATAL_ERROR "The compiler ${CMAKE_CXX_COMPILER} has no C++11 support. Please use a different C++ compiler.")
+		endif()
+	else()
+		CHECK_CXX_COMPILER_FLAG("-std=c++11" COMPILER_SUPPORTS_CXX11)
+		CHECK_CXX_COMPILER_FLAG("-std=c++0x" COMPILER_SUPPORTS_CXX0X)
+		if(COMPILER_SUPPORTS_CXX11)
+			set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -std=c++11")
+		elseif(COMPILER_SUPPORTS_CXX0X)
+			set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -std=c++0x")
+		else()
+				message(FATAL_ERROR "The compiler ${CMAKE_CXX_COMPILER} has no C++11 support. Please use a different C++ compiler.")
+		endif()
+	endif()
+endmacro()
---
+++ src/core/util/Compressor.h
@@ -0,0 +1,41 @@
+/**
+* Copyright 2018 Dynatrace LLC
+*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+*     http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*/
+
+#ifndef _CORE_UTIL_COMPRESSOR_H
+#define _CORE_UTIL_COMPRESSOR_H
+
+#include <vector>
+#include <cstddef>
+
+namespace base
+{
+	///
+	/// Utility class to compress data with zlib
+	///
+	class Compressor
+	{
+	public:
+
+		///
+		/// Compress block of memory at in_data with a length of in_data_size bytes 
+		/// @param[in] in_data pointer to the incoming data
+		/// @param[in] in_data_size size of data behind the pointer (measured in bytes)
+		/// @out_data[out] binary_data struct passed as reference that will contain the compressed data.
+		///
+		static void compressMemory(const void *inData, size_t inDataSize, std::vector<unsigned char>& out_data);
+	};
+}
+#endif
