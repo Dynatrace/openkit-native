664367bd694c20518b71b88dfd5d610460ab6d03
Johannes Baeuerle <johannes.baeuerle@dynatrace.com>
2018-02-07 18:22:47 +0100

    add DefaultHTTPClientProvider

---
+++ src/providers/DefaultHTTPClientProvider.h
@@ -0,0 +1,39 @@
+/**
+* Copyright 2018 Dynatrace LLC
+*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+*     http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*/
+
+#ifndef _PROVIDERS_DEFAULTTIMINGPROVIDER_H
+#define _PROVIDERS_DEFAULTTIMINGPROVIDER_H
+
+#include "providers/IHTTPClientProvider.h"
+
+#include "configuration/HTTPClientConfiguration.h"
+
+namespace providers {
+	///
+	/// Implementation of an HTTPClientProvider which creates a HTTP client for executing status check, beacon send and time sync requests.
+	///
+	class DefaultHTTPClientProvider : public IHTTPClientProvider
+	{
+	public:
+		///
+		/// Returns an HTTPClient based on the provided configuration.
+		/// @param[in] configuration configuration parameters for the HTTP connection
+		///
+		virtual protocol::HTTPClient* createClient(configuration::HTTPClientConfiguration configuration) override;
+	};
+}
+
+#endif
