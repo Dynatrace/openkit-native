7ff49b425acc95e55b65c475cbcfb202ae46989d
Johannes Baeuerle <johannes.baeuerle@dynatrace.com>
2018-02-09 11:53:03 +0100

    implement CountDownLatch based on condition var
    similar to java implementation. condition variable is used
    to achieve a similar behavior. threads can perform a blocking
    wait until a countdown reaches 0.

---
+++ src/core/util/CountDownLatch.cxx
@@ -0,0 +1,52 @@
+/**
+* Copyright 2018 Dynatrace LLC
+*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+*     http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*/
+
+#include "CountDownLatch.h"
+
+using namespace core::util;
+
+CountDownLatch::CountDownLatch()
+	: CountDownLatch(1)
+{
+}
+
+CountDownLatch::CountDownLatch(uint32_t initial)
+{
+	std::unique_lock<std::mutex> lck(mMutex);
+	mCount = initial;
+}
+
+void CountDownLatch::countDown()
+{
+	std::unique_lock<std::mutex> lock(mMutex);
+
+	mCount--;
+	if (mCount == 0)
+	{
+		mConditionVariable.notify_all();
+	}
+
+}
+
+void CountDownLatch::await()
+{
+	std::unique_lock<std::mutex> lock(mMutex);
+
+	while (mCount > 0)
+	{
+		mConditionVariable.wait(lock);
+	}
+}
