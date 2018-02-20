6e9eec16c61fb01472c8c7552d53ff7220733603
Roland Ettinger <roland.ettinger@dynatrace.com>
2018-02-20 15:53:33 +0100

    Work in review comment

--- src/core/util/Compressor.cxx
+++ src/core/util/Compressor.cxx
@@ -18,6 +18,7 @@
 
 #include <algorithm>
 #include <zlib.h>
+#include "assert.h"
 
 using namespace base::util;
 
@@ -29,14 +30,14 @@ void Compressor::compressMemory(const void* inData, size_t inDataSize, std::vect
 	std::vector<uint8_t> buffer;
 
 	const size_t BUFSIZE = 128 * 1024;
-	uint8_t temp_buffer[BUFSIZE];
+	uint8_t tmpBuffer[BUFSIZE];
 
 	z_stream strm;
 	strm.zalloc = 0;
 	strm.zfree = 0;
 	strm.next_in = (Bytef*)inData;
 	strm.avail_in = inDataSize;
-	strm.next_out = temp_buffer;
+	strm.next_out = tmpBuffer;
 	strm.avail_out = BUFSIZE;
 
 	// Use GZIP with default compresssion
@@ -45,29 +46,29 @@ void Compressor::compressMemory(const void* inData, size_t inDataSize, std::vect
 	while (strm.avail_in != 0)
 	{
 		int res = deflate(&strm, Z_NO_FLUSH);
-		//assert(res == Z_OK);
+		assert(res == Z_OK);
 		if (strm.avail_out == 0)
 		{
-			buffer.insert(buffer.end(), temp_buffer, temp_buffer + BUFSIZE);
-			strm.next_out = temp_buffer;
+			buffer.insert(buffer.end(), tmpBuffer, tmpBuffer + BUFSIZE);
+			strm.next_out = tmpBuffer;
 			strm.avail_out = BUFSIZE;
 		}
 	}
 
-	int deflate_res = Z_OK;
-	while (deflate_res == Z_OK)
+	uint32_t deflateResult = Z_OK;
+	do
 	{
 		if (strm.avail_out == 0)
 		{
-			buffer.insert(buffer.end(), temp_buffer, temp_buffer + BUFSIZE);
-			strm.next_out = temp_buffer;
+			buffer.insert(buffer.end(), tmpBuffer, tmpBuffer + BUFSIZE);
+			strm.next_out = tmpBuffer;
 			strm.avail_out = BUFSIZE;
 		}
-		deflate_res = deflate(&strm, Z_FINISH);
-	}
+		deflateResult = deflate(&strm, Z_FINISH);
+	} while (deflateResult == Z_OK);
 
-	//assert(deflate_res == Z_STREAM_END);
-	buffer.insert(buffer.end(), temp_buffer, temp_buffer + BUFSIZE - strm.avail_out);
+	assert(deflateResult == Z_STREAM_END);
+	buffer.insert(buffer.end(), tmpBuffer, tmpBuffer + BUFSIZE - strm.avail_out);
 	deflateEnd(&strm);
 
 	outData.swap(buffer);
