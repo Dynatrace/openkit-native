59000762754c304f82e9a9ec305e3e1e61ad7855
Roland Ettinger <roland.ettinger@dynatrace.com>
2018-02-21 08:02:29 +0100

    Add parsing of StatusResponse

--- src/protocol/StatusResponse.cxx
+++ src/protocol/StatusResponse.cxx
@@ -25,6 +25,7 @@ constexpr char RESPONSE_KEY_CAPTURE_CRASHES[] = "cr";
 #include "StatusResponse.h"
 
 #include <stdexcept>
+#include <sstream>
 
 using namespace protocol;
 
@@ -55,7 +56,51 @@ StatusResponse::StatusResponse(const core::UTF8String& response, uint32_t respon
 
 void StatusResponse::parseResponse(const core::UTF8String& response)
 {
+	std::stringstream ss(response.getStringData());
+	std::string item;
+	while (std::getline(ss, item, '&'))
+	{
+		size_t found = item.find('=');
+		if (found != std::string::npos)
+		{
+			std::string key = item.substr(0, found);
+			std::string value = item.substr(found + 1);
 
+			if (!key.empty() && !value.empty())
+			{
+				if (key.compare(RESPONSE_KEY_CAPTURE) == 0)
+				{
+					mCapture = std::stoi(value) == 1;
+				}
+				else if (key.compare(RESPONSE_KEY_SEND_INTERVAL) == 0)
+				{
+					mSendInterval = std::stoi(value) * 1000;
+				}
+				else if (key.compare(RESPONSE_KEY_MONITOR_NAME) == 0)
+				{
+					mMonitorName = core::UTF8String(value.c_str());
+				}
+				else if (key.compare(RESPONSE_KEY_SERVER_ID) == 0)
+				{
+					mServerID = std::stoi(value);
+				}
+				else if (key.compare(RESPONSE_KEY_MAX_BEACON_SIZE) == 0)
+				{
+					mMaxBeaconSize = std::stoi(value);
+				}
+				else if (key.compare(RESPONSE_KEY_CAPTURE_ERRORS) == 0)
+				{
+					/* 1 (always on) and 2 (only on WiFi) are treated the same */
+					mCaptureErrors = std::stoi(value) != 0;
+				}
+				else if (key.compare(RESPONSE_KEY_CAPTURE_CRASHES) == 0)
+				{
+					/* 1 (always on) and 2 (only on WiFi) are treated the same */
+					mCaptureCrashes = std::stoi(value) != 0;
+				}
+			}
+		}
+	}
 }
 
 bool StatusResponse::isCapture() const
---
+++ test/protocol/StatusResponseTest.cxx
@@ -0,0 +1,81 @@
+/**
+* Copyright 2018 Dynatrace LLC
+*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+*     http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*/
+#include "protocol/StatusResponse.h"
+
+#include <cstdint>
+#include <gtest/gtest.h>
+
+using namespace core;
+using namespace protocol;
+
+class StatusResponseTest : public testing::Test
+{
+public:
+	void SetUp()
+	{
+
+	}
+
+	void TearDown()
+	{
+
+	}
+};
+
+TEST_F(StatusResponseTest, AllDefaultsStatusResponse)
+{
+	UTF8String s("");
+	uint32_t responseCode = 200;
+	StatusResponse statusResponse = StatusResponse(s, responseCode);
+
+	EXPECT_TRUE(statusResponse.isCapture());
+	EXPECT_EQ(-1, statusResponse.getSendInterval());
+	EXPECT_TRUE(statusResponse.getMonitorName().empty());
+	EXPECT_EQ(-1, statusResponse.getServerID());
+	EXPECT_EQ(-1, statusResponse.getMaxBeaconSize());
+	EXPECT_TRUE(statusResponse.isCaptureErrors());
+	EXPECT_TRUE(statusResponse.isCaptureCrashes());
+}
+
+TEST_F(StatusResponseTest, SomeTypicalStatusResponse)
+{
+	UTF8String s("cp=1&si=2&bn=MyName&id=5&bl=3072&er=1&cr=0");
+	uint32_t responseCode = 200;
+	StatusResponse statusResponse = StatusResponse(s, responseCode);
+
+	EXPECT_TRUE(statusResponse.isCapture());
+	EXPECT_EQ(2000, statusResponse.getSendInterval());
+	EXPECT_TRUE(statusResponse.getMonitorName().compare("MyName"));
+	EXPECT_EQ(5, statusResponse.getServerID());
+	EXPECT_EQ(3072, statusResponse.getMaxBeaconSize());
+	EXPECT_TRUE(statusResponse.isCaptureErrors());
+	EXPECT_FALSE(statusResponse.isCaptureCrashes());
+}
+
+TEST_F(StatusResponseTest, CorruptStatusResponse)
+{
+	UTF8String s("cp=");
+	uint32_t responseCode = 200;
+	StatusResponse statusResponse = StatusResponse(s, responseCode);
+
+	EXPECT_TRUE(statusResponse.isCapture());
+	EXPECT_EQ(-1, statusResponse.getSendInterval());
+	EXPECT_TRUE(statusResponse.getMonitorName().empty());
+	EXPECT_EQ(-1, statusResponse.getServerID());
+	EXPECT_EQ(-1, statusResponse.getMaxBeaconSize());
+	EXPECT_TRUE(statusResponse.isCaptureErrors());
+	EXPECT_TRUE(statusResponse.isCaptureCrashes());
+}
