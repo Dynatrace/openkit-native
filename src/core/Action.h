c30d77aec7dcb25b53b8360b345164ebf3495355
Johannes Baeuerle <johannes.baeuerle@dynatrace.com>
2018-03-02 11:33:24 +0100

    provide interfaces to create Actions in a Session

---
+++ src/core/Action.h
@@ -0,0 +1,44 @@
+/**
+* Copyright 2018 Dynatrace LLC
+*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+*     http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*/
+
+#ifndef _CORE_ACTION_H
+#define _CORE_ACTION_H
+
+#include "api/IAction.h"
+
+#include "memory"
+
+namespace core
+{
+	///
+	/// Actual implementation of the IAction interface.
+	///
+	class Action : public api::IAction
+	{
+	public:
+		///
+		/// Destructor
+		///
+		virtual ~Action() {};
+
+		///
+		/// Leaves this Action.
+		/// @returns the parent Action, or @c null if there is no parent Action
+		///
+		virtual std::shared_ptr<api::IAction> LeaveAction();
+	};
+}
+#endif
