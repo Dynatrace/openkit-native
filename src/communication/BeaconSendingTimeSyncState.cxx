c9d92f81b93c73ebeb31dca0f9be05c4ea36bc63
Johannes Baeuerle <johannes.baeuerle@dynatrace.com>
2018-06-04 15:57:02 +0200

    sanitize list of time offsets to avoid div by 0

--- src/communication/BeaconSendingTimeSyncState.cxx
+++ src/communication/BeaconSendingTimeSyncState.cxx
@@ -121,8 +121,14 @@ void BeaconSendingTimeSyncState::handleTimeSyncResponses(BeaconSendingContext& c
 		return;
 	}
 
+	auto calculatedOffset = computeClusterTimeOffset(timeSyncOffsets);
+	if (calculatedOffset < 0)
+	{
+		return;
+	}
+
 	// initialize time provider with cluster time offset
-	context.initializeTimeSync(computeClusterTimeOffset(timeSyncOffsets), true);
+	context.initializeTimeSync(calculatedOffset, true);
 
 	// also update the time when last time sync was performed to now
 	context.setLastTimeSyncTime(context.getCurrentTimestamp());
@@ -159,6 +165,11 @@ int64_t BeaconSendingTimeSyncState::computeClusterTimeOffset(std::vector<int64_t
 			sum += timeSyncOffsets[i];
 			count++;
 		}
+	}
+
+	if (count == 0)
+	{
+		return 0;
 	}
 
 	return static_cast<int64_t>(std::round(sum / static_cast<double>(count)));
