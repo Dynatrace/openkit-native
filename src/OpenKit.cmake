4123e619af6b7769513db2ad6ff8e1f18198e724
Stefan Eberl <stefan.eberl@dynatrace.com>
2018-08-21 10:27:38 +0200

    Untabify all lines

--- src/OpenKit.cmake
+++ src/OpenKit.cmake
@@ -14,19 +14,19 @@
 
 set(OPENKIT_PUBLIC_HEADERS_CXX_API
     ${CMAKE_SOURCE_DIR}/include/OpenKit/AbstractOpenKitBuilder.h
-	${CMAKE_SOURCE_DIR}/include/OpenKit/AppMonOpenKitBuilder.h
+    ${CMAKE_SOURCE_DIR}/include/OpenKit/AppMonOpenKitBuilder.h
     ${CMAKE_SOURCE_DIR}/include/OpenKit/CrashReportingLevel.h
-	${CMAKE_SOURCE_DIR}/include/OpenKit/DataCollectionLevel.h
-	${CMAKE_SOURCE_DIR}/include/OpenKit/DynatraceOpenKitBuilder.h
+    ${CMAKE_SOURCE_DIR}/include/OpenKit/DataCollectionLevel.h
+    ${CMAKE_SOURCE_DIR}/include/OpenKit/DynatraceOpenKitBuilder.h
     ${CMAKE_SOURCE_DIR}/include/OpenKit/IAction.h
-	${CMAKE_SOURCE_DIR}/include/OpenKit/ILogger.h
-	${CMAKE_SOURCE_DIR}/include/OpenKit/IOpenKit.h
-	${CMAKE_SOURCE_DIR}/include/OpenKit/IRootAction.h
-	${CMAKE_SOURCE_DIR}/include/OpenKit/ISession.h
-	${CMAKE_SOURCE_DIR}/include/OpenKit/ISSLTrustManager.h
-	${CMAKE_SOURCE_DIR}/include/OpenKit/IWebRequestTracer.h
+    ${CMAKE_SOURCE_DIR}/include/OpenKit/ILogger.h
+    ${CMAKE_SOURCE_DIR}/include/OpenKit/IOpenKit.h
+    ${CMAKE_SOURCE_DIR}/include/OpenKit/IRootAction.h
+    ${CMAKE_SOURCE_DIR}/include/OpenKit/ISession.h
+    ${CMAKE_SOURCE_DIR}/include/OpenKit/ISSLTrustManager.h
+    ${CMAKE_SOURCE_DIR}/include/OpenKit/IWebRequestTracer.h
     ${CMAKE_SOURCE_DIR}/include/OpenKit/OpenKitConstants.h
-	${CMAKE_SOURCE_DIR}/include/OpenKit.h
+    ${CMAKE_SOURCE_DIR}/include/OpenKit.h
 )
 
 set(OPENKIT_PUBLIC_HEADERS_C_API
@@ -35,13 +35,13 @@ set(OPENKIT_PUBLIC_HEADERS_C_API
 
 set(OPENKIT_SOURCES_CXX_API
     ${CMAKE_CURRENT_LIST_DIR}/api/AbstractOpenKitBuilder.cxx
-	${CMAKE_CURRENT_LIST_DIR}/api/AppMonOpenKitBuilder.cxx
+    ${CMAKE_CURRENT_LIST_DIR}/api/AppMonOpenKitBuilder.cxx
     ${CMAKE_CURRENT_LIST_DIR}/api/DynatraceOpenKitBuilder.cxx
 )
 
 set(OPENKIT_SOURCES_C_API
     ${CMAKE_CURRENT_LIST_DIR}/api-c/CustomLogger.cxx
-	${CMAKE_CURRENT_LIST_DIR}/api-c/CustomLogger.h
+    ${CMAKE_CURRENT_LIST_DIR}/api-c/CustomLogger.h
     ${CMAKE_CURRENT_LIST_DIR}/api-c/CustomTrustManager.h
     ${CMAKE_CURRENT_LIST_DIR}/api-c/CustomTrustManager.cxx
     ${CMAKE_CURRENT_LIST_DIR}/api-c/OpenKit-c.cxx
@@ -56,8 +56,8 @@ set(OPENKIT_SOURCES_CACHING
     ${CMAKE_CURRENT_LIST_DIR}/caching/BeaconCacheEvictor.h
     ${CMAKE_CURRENT_LIST_DIR}/caching/BeaconCacheRecord.cxx
     ${CMAKE_CURRENT_LIST_DIR}/caching/BeaconCacheRecord.h
-	${CMAKE_CURRENT_LIST_DIR}/caching/IBeaconCache.h
-	${CMAKE_CURRENT_LIST_DIR}/caching/IObserver.h
+    ${CMAKE_CURRENT_LIST_DIR}/caching/IBeaconCache.h
+    ${CMAKE_CURRENT_LIST_DIR}/caching/IObserver.h
     ${CMAKE_CURRENT_LIST_DIR}/caching/SpaceEvictionStrategy.cxx
     ${CMAKE_CURRENT_LIST_DIR}/caching/SpaceEvictionStrategy.h
     ${CMAKE_CURRENT_LIST_DIR}/caching/TimeEvictionStrategy.cxx
@@ -112,11 +112,11 @@ set(OPENKIT_SOURCES_CORE_UTIL
     ${CMAKE_CURRENT_LIST_DIR}/core/util/InetAddressValidator.cxx
     ${CMAKE_CURRENT_LIST_DIR}/core/util/InetAddressValidator.h
     ${CMAKE_CURRENT_LIST_DIR}/core/util/ReadWriteLock.h
-	${CMAKE_CURRENT_LIST_DIR}/core/util/ScopedReadLock.h
-	${CMAKE_CURRENT_LIST_DIR}/core/util/ScopedWriteLock.h
-	${CMAKE_CURRENT_LIST_DIR}/core/util/SynchronizedQueue.h
+    ${CMAKE_CURRENT_LIST_DIR}/core/util/ScopedReadLock.h
+    ${CMAKE_CURRENT_LIST_DIR}/core/util/ScopedWriteLock.h
+    ${CMAKE_CURRENT_LIST_DIR}/core/util/SynchronizedQueue.h
     ${CMAKE_CURRENT_LIST_DIR}/core/util/URLEncoding.cxx
-	${CMAKE_CURRENT_LIST_DIR}/core/util/URLEncoding.h
+    ${CMAKE_CURRENT_LIST_DIR}/core/util/URLEncoding.h
 )
 
 set(OPENKIT_SOURCES_CORE
@@ -126,18 +126,18 @@ set(OPENKIT_SOURCES_CORE
     ${CMAKE_CURRENT_LIST_DIR}/core/ActionCommonImpl.h
     ${CMAKE_CURRENT_LIST_DIR}/core/BeaconSender.cxx
     ${CMAKE_CURRENT_LIST_DIR}/core/BeaconSender.h
-	${CMAKE_CURRENT_LIST_DIR}/core/NullAction.h
-	${CMAKE_CURRENT_LIST_DIR}/core/NullRootAction.h
-	${CMAKE_CURRENT_LIST_DIR}/core/NullSession.h
-	${CMAKE_CURRENT_LIST_DIR}/core/NullWebRequestTracer.h
+    ${CMAKE_CURRENT_LIST_DIR}/core/NullAction.h
+    ${CMAKE_CURRENT_LIST_DIR}/core/NullRootAction.h
+    ${CMAKE_CURRENT_LIST_DIR}/core/NullSession.h
+    ${CMAKE_CURRENT_LIST_DIR}/core/NullWebRequestTracer.h
     ${CMAKE_CURRENT_LIST_DIR}/core/OpenKit.cxx
     ${CMAKE_CURRENT_LIST_DIR}/core/OpenKit.h
     ${CMAKE_CURRENT_LIST_DIR}/core/RootAction.cxx
     ${CMAKE_CURRENT_LIST_DIR}/core/RootAction.h
     ${CMAKE_CURRENT_LIST_DIR}/core/Session.cxx
     ${CMAKE_CURRENT_LIST_DIR}/core/Session.h
-	${CMAKE_CURRENT_LIST_DIR}/core/SessionWrapper.cxx
-	${CMAKE_CURRENT_LIST_DIR}/core/SessionWrapper.h
+    ${CMAKE_CURRENT_LIST_DIR}/core/SessionWrapper.cxx
+    ${CMAKE_CURRENT_LIST_DIR}/core/SessionWrapper.h
     ${CMAKE_CURRENT_LIST_DIR}/core/UTF8String.cxx
     ${CMAKE_CURRENT_LIST_DIR}/core/UTF8String.h
     ${CMAKE_CURRENT_LIST_DIR}/core/WebRequestTracerBase.cxx
@@ -156,13 +156,13 @@ set(OPENKIT_SOURCES_PROTOCOL_SSL
 set(OPENKIT_SOURCES_PROTOCOL
     ${CMAKE_CURRENT_LIST_DIR}/protocol/Beacon.cxx
     ${CMAKE_CURRENT_LIST_DIR}/protocol/Beacon.h
-	${CMAKE_CURRENT_LIST_DIR}/protocol/BeaconProtocolConstants.h
-	${CMAKE_CURRENT_LIST_DIR}/protocol/EventType.h
+    ${CMAKE_CURRENT_LIST_DIR}/protocol/BeaconProtocolConstants.h
+    ${CMAKE_CURRENT_LIST_DIR}/protocol/EventType.h
     ${CMAKE_CURRENT_LIST_DIR}/protocol/HTTPClient.cxx
     ${CMAKE_CURRENT_LIST_DIR}/protocol/HTTPClient.h
-	${CMAKE_CURRENT_LIST_DIR}/protocol/HTTPResponseParser.cxx
-	${CMAKE_CURRENT_LIST_DIR}/protocol/HTTPResponseParser.h
-	${CMAKE_CURRENT_LIST_DIR}/protocol/IHTTPClient.h
+    ${CMAKE_CURRENT_LIST_DIR}/protocol/HTTPResponseParser.cxx
+    ${CMAKE_CURRENT_LIST_DIR}/protocol/HTTPResponseParser.h
+    ${CMAKE_CURRENT_LIST_DIR}/protocol/IHTTPClient.h
     ${CMAKE_CURRENT_LIST_DIR}/protocol/Response.cxx
     ${CMAKE_CURRENT_LIST_DIR}/protocol/Response.h
     ${CMAKE_CURRENT_LIST_DIR}/protocol/StatusResponse.cxx
@@ -200,7 +200,7 @@ set(OPENKIT_SOURCES
     ${OPENKIT_SOURCES_CORE_UTIL}
     ${OPENKIT_SOURCES_COMMUNICATION}
     ${OPENKIT_SOURCES_PROVIDERS}
-	${OPENKIT_SOURCES_PROTOCOL_SSL}
+    ${OPENKIT_SOURCES_PROTOCOL_SSL}
     ${OPENKIT_SOURCES_PROTOCOL}
     ${OPENKIT_SOURCES_CONFIGURATION}
 )
@@ -209,64 +209,64 @@ include(CompilerConfiguration)
 fix_compiler_flags()
 
 function(build_open_kit)
-	message("Configuring OpenKit ... ")
+    message("Configuring OpenKit ... ")
 
-	find_package(ZLIB)
-	find_package(CURL)
+    find_package(ZLIB)
+    find_package(CURL)
 
-	set(OPENKIT_INCLUDE_DIRS
-		${ZLIB_INCLUDE_DIR}
-		${CURL_INCLUDE_DIR}
-		${CMAKE_CURRENT_SOURCE_DIR}/include
-		${CMAKE_CURRENT_SOURCE_DIR}/src
-		${CMAKE_BINARY_DIR}/include
-	)
+    set(OPENKIT_INCLUDE_DIRS
+        ${ZLIB_INCLUDE_DIR}
+        ${CURL_INCLUDE_DIR}
+        ${CMAKE_CURRENT_SOURCE_DIR}/include
+        ${CMAKE_CURRENT_SOURCE_DIR}/src
+        ${CMAKE_BINARY_DIR}/include
+    )
 
-	set(OPENKIT_LIBS
-		${ZLIB_LIBRARY}
-		${CURL_LIBRARY}
-	)
+    set(OPENKIT_LIBS
+        ${ZLIB_LIBRARY}
+        ${CURL_LIBRARY}
+    )
 
-	include(CompilerConfiguration)
-	include(BuildFunctions)
+    include(CompilerConfiguration)
+    include(BuildFunctions)
 
-	
-	open_kit_build_library(OpenKit "${OPENKIT_INCLUDE_DIRS}" "${OPENKIT_LIBS}" ${OPENKIT_SOURCES})
+    
+    open_kit_build_library(OpenKit "${OPENKIT_INCLUDE_DIRS}" "${OPENKIT_LIBS}" ${OPENKIT_SOURCES})
 
-	# enforce usage of C++11 for OpenKit
-	enforce_cxx11_standard(OpenKit)
+    # enforce usage of C++11 for OpenKit
+    enforce_cxx11_standard(OpenKit)
 
-	# add special processor flag when building OpenKit as static library
-	if(NOT BUILD_SHARED_LIBS)
-		# For a static library we set the compiler flag OPENKIT_STATIC_DEFINE
-		target_compile_definitions(OpenKit PRIVATE -DOPENKIT_STATIC_DEFINE)
-	endif()
+    # add special processor flag when building OpenKit as static library
+    if(NOT BUILD_SHARED_LIBS)
+        # For a static library we set the compiler flag OPENKIT_STATIC_DEFINE
+        target_compile_definitions(OpenKit PRIVATE -DOPENKIT_STATIC_DEFINE)
+    endif()
 
-	# add special preprocessor flag when curl is used as static library
-	if (NOT BUILD_SHARED_LIBS OR OPENKIT_MONOLITHIC_SHARED_LIB)
-		target_compile_definitions(OpenKit PRIVATE -DCURL_STATICLIB)
-	endif()
+    # add special preprocessor flag when curl is used as static library
+    if (NOT BUILD_SHARED_LIBS OR OPENKIT_MONOLITHIC_SHARED_LIB)
+        target_compile_definitions(OpenKit PRIVATE -DCURL_STATICLIB)
+    endif()
 
-	include(GenerateExportHeader)
-	generate_export_header(OpenKit
-		BASE_NAME OpenKit
-		EXPORT_MACRO_NAME OPENKIT_EXPORT
-		EXPORT_FILE_NAME ${CMAKE_BINARY_DIR}/include/OpenKit_export.h
-		STATIC_DEFINE OPENKIT_STATIC_DEFINE
-	)
+    include(GenerateExportHeader)
+    generate_export_header(OpenKit
+        BASE_NAME OpenKit
+        EXPORT_MACRO_NAME OPENKIT_EXPORT
+        EXPORT_FILE_NAME ${CMAKE_BINARY_DIR}/include/OpenKit_export.h
+        STATIC_DEFINE OPENKIT_STATIC_DEFINE
+    )
 
-	# Add a make target "install". If called (e.g. with "make install") this installs the library files
-	install(TARGETS OpenKit
-		ARCHIVE DESTINATION "${INSTALL_LIB_DIR}"
-		LIBRARY DESTINATION "${INSTALL_LIB_DIR}"
-		RUNTIME DESTINATION "${INSTALL_BIN_DIR}"
-	)
-	# This installs the public header files and the export.h file
-	install(FILES
-		${PUBLIC_HEADERS}
-		${CMAKE_BINARY_DIR}/include/OpenKit_export.h
-		DESTINATION "${INSTALL_INC_DIR}"
-	)
+    # Add a make target "install". If called (e.g. with "make install") this installs the library files
+    install(TARGETS OpenKit
+        ARCHIVE DESTINATION "${INSTALL_LIB_DIR}"
+        LIBRARY DESTINATION "${INSTALL_LIB_DIR}"
+        RUNTIME DESTINATION "${INSTALL_BIN_DIR}"
+    )
+    # This installs the public header files and the export.h file
+    install(FILES
+        ${PUBLIC_HEADERS}
+        ${CMAKE_BINARY_DIR}/include/OpenKit_export.h
+        DESTINATION "${INSTALL_INC_DIR}"
+    )
 
     set_target_properties(OpenKit PROPERTIES FOLDER Lib)
     source_group("Header Files\\API - C" FILES ${OPENKIT_PUBLIC_HEADERS_C_API})
@@ -278,7 +278,7 @@ function(build_open_kit)
     source_group("Source Files\\Core\\Util" FILES ${OPENKIT_SOURCES_CORE_UTIL})
     source_group("Source Files\\Communication" FILES ${OPENKIT_SOURCES_COMMUNICATION})
     source_group("Source Files\\Protocol" FILES ${OPENKIT_SOURCES_PROTOCOL})
-	source_group("Source Files\\Protocol\\SSL" FILES ${OPENKIT_SOURCES_PROTOCOL_SSL})
+    source_group("Source Files\\Protocol\\SSL" FILES ${OPENKIT_SOURCES_PROTOCOL_SSL})
     source_group("Source Files\\Providers" FILES ${OPENKIT_SOURCES_PROVIDERS})
     source_group("Source Files\\Configuration" FILES ${OPENKIT_SOURCES_CONFIGURATION})
 
