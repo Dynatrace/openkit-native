7d2ac4c34422739f306103e9239a564dfabad58a
Stefan Eberl <stefan.eberl@dynatrace.com>
2018-11-22 15:23:58 +0100

    Fix initialization of string in initializer list

--- src/protocol/Beacon.cxx
+++ src/protocol/Beacon.cxx
@@ -47,7 +47,7 @@ Beacon::Beacon(std::shared_ptr<openkit::ILogger> logger, std::shared_ptr<caching
 	, mBeaconCache(beaconCache)
 	, mHTTPClientConfiguration(configuration->getHTTPClientConfiguration())
 	, mBeaconConfiguration(configuration->getBeaconConfiguration())
-	, mDeviceID(nullptr)
+	, mDeviceID("")
 	, mRandomGenerator(randomGenerator)
 {
 	if (core::util::InetAddressValidator::IsValidIP(clientIPAddress))
