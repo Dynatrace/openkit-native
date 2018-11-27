5f28f455377879e7c58ac05a95b6e2c42f18fb09
Stefan Eberl <stefan.eberl@dynatrace.com>
2018-11-27 15:19:15 +0100

    Fix typo and remove some unused whitespaces

--- test/configuration/ConfigurationTest.cxx
+++ test/configuration/ConfigurationTest.cxx
@@ -129,7 +129,6 @@ TEST_F(ConfigurationTest, capturingIsDisabledFromStatusResponse)
 	ASSERT_FALSE(target->isCapture());
 }
 
-
 TEST_F(ConfigurationTest, tenantURLisSetCorrectly)
 {
 	core::UTF8String host("localhost:9999");
@@ -141,7 +140,6 @@ TEST_F(ConfigurationTest, tenantURLisSetCorrectly)
 	auto target = getConfiguration(tenantURL);
 
 	ASSERT_TRUE(tenantURL.equals(target->getHTTPClientConfiguration()->getBaseURL()));
-
 }
 
 TEST_F(ConfigurationTest, defaultDataCollectionLevelIsDefaultValueFromBeaconConfiguration)
@@ -154,4 +152,22 @@ TEST_F(ConfigurationTest, defaultCrashReportingLevelIsDefaultValueFromBeaconConf
 {
 	auto target = getDefaultConfiguration();
 	ASSERT_EQ(target->getBeaconConfiguration()->getCrashReportingLevel(), configuration::BeaconConfiguration::DEFAULT_CRASH_REPORTING_LEVEL);
+}
+
+TEST_F(ConfigurationTest, getApplicationID)
+{
+	// given
+	auto target = getDefaultConfiguration();
+
+	// then
+	ASSERT_EQ(target->getApplicationID(), "/App_ID%");
+}
+
+TEST_F(ConfigurationTest, getApplicationIDPercentEncodedDoesProperEncoding)
+{
+	// given
+	auto target = getDefaultConfiguration();
+
+	// then
+	ASSERT_EQ(target->getApplicationIDPercentEncoded(), "%2FApp%5FID%25");
 }
